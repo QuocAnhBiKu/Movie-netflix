90221e753cd6a7e8a6febddb5196907b90de39b1
QuocAnh <anhbr2016@gmail.com>
2024-11-12 14:25:28 +0700

    success protect routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"
 import movieRoutes from "./routes/movie.routes.js"
 import tvRoutes from "./routes/tv.routes.js";
+import searchRoutes from "./routes/search.routes.js"
+
+import {protectRoute} from "./middleware/protectRoute.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -9,10 +14,12 @@ const app = express();
 const PORT = ENV_VARS.PORT
 
 app.use(express.json());// will allow us to parse req.body
+app.use(cookieParser());
 
 app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movie",movieRoutes);
-app.use("/api/v1/tv",tvRoutes);
+app.use("/api/v1/movie",protectRoute ,movieRoutes);
+app.use("/api/v1/tv", protectRoute,tvRoutes);
+app.use("/api/v1/search", protectRoute,searchRoutes);
 
 
 app.listen(PORT,()=>{
